930ac137192283bddbd77113e405b8bcb7d540e4
hszilvi <szilvi@naszta.hu>
2024-04-03 16:26:15 +0100

    add some more js logic

--- script.js
+++ script.js
@@ -12,11 +12,14 @@ const grid = document.querySelector('.board')
 // array for numbers 1 to 18 
 const numbersSmallArray = ['1', '2', '3', '4', '5', '6', '7', '8', '1', '2', '3', '4', '5', '6', '7', '8']
 const numbersBigArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18']
+// array for characters 
+const iconsSmallArray = [];
+const iconsBigArray = [];
 
 // options chosen
 let themeChosen;
-let playerChosen = [];
-let gridChosen = [];
+let playerChosen;
+let gridChosen;
 // create grid
 let cardChosen = [];
 let cardChosenId = [];
@@ -28,29 +31,89 @@ numbersBigArray.sort(() => 0.5 - Math.random());
 
 // function to check which theme button is clicked
 const theme = document.getElementById('number-btn');
+const theme2 = document.getElementById('icon-btn');
 function checkTheme() {
-    // let theme2 = document.getElementById('icon-btn');
-    console.log(theme);
-        if (JSON.stringify(theme) === 'number-btn') {
+        if (theme.classList.contains('clicked')) {
             console.log('its a number')
         themeChosen = 'Numbers';
-    } else {
-    
+    } else if (theme2.classList.contains('clicked')) {   
         themeChosen = 'Icons';
     }
 };
 
-for (let i = 0; i < themeBtn.length; i++) {
-
-
-    themeBtn[i].addEventListener('click', () => {
+themeBtn.forEach(button => {
+    button.addEventListener('click', () => {
+        button.classList.add('clicked');
+        // themeBtn.forEach(btn => {
+        //     if (btn !== button) {
+        //         btn.classList.remove('clicked');
+        //     }
+        // });
         checkTheme();
-        console.log('theme clicked')
-    
         console.log(themeChosen);
     });
+});
+// check how many players
+const onePlayer = document.getElementById('player1');
+const twoPlayer = document.getElementById('player2');
+const threePlayer = document.getElementById('player3');
+const fourPlayer = document.getElementById('player4');
+
+function checkPlayers() {
+    if (onePlayer.classList.contains('clicked')) {
+        playerChosen = 1;
+
+    } else if (twoPlayer.classList.contains('clicked')) {
+        playerChosen = 2;
+    } else if (threePlayer.classList.contains('clicked')) {
+        playerChosen = 3;
+    } else if (fourPlayer.classList.contains('clicked')) {
+        playerChosen = 4;
+    }
+console.log(playerChosen)
 }
 
+playersNumber.forEach(button => {
+    button.addEventListener('click', () => {
+        button.classList.add('clicked');
+        // playersNumber.forEach(btn => {
+        //     if (btn !== button) {
+        //         btn.classList.remove('clicked');
+        //     }
+        // });
+        checkPlayers();
+    });
+});
+// check grid size
+const smallGrid = document.getElementById('grid4');
+const bigGrid = document.getElementById('grid6');
+
+function checkGridSize() {
+    if (smallGrid.classList.contains('clicked')) {
+        gridChosen = 'small';
+    } else if (bigGrid.classList.contains('clicked')) {
+        gridChosen = 'big';
+    };
+    console.log(gridChosen);
+}
+gridSize.forEach(button => {
+    button.addEventListener('click', () => {
+        button.classList.add('clicked');
+        // gridSize.forEach(btn => {
+        //     if (btn !== button) {
+
+        //         btn.classList.remove('clicked');
+        //     }
+        // });
+        checkGridSize();
+    });
+});
+// create players card
+function createPlayersCards() {
+    if (playerChosen === 1) {
+        document.getElementsByClassName('single-player-board').style.display = "block";
+    }
+}
 
 // create game board
 function createSmallBoard() {
@@ -60,9 +123,29 @@ function createSmallBoard() {
         card.innerHTML = numbersSmallArray[i];
         card.className = 'card';
         grid.appendChild(card);
-    }
-}
-startGameBtn.addEventListener('click', () => {
+    };
+};
+function createBigBoard() {
+    for (let i= 0; i < numbersBigArray.length; i++) {
+        const card = document.createElement('div');
+        card.setAttribute('data-id', i);
+        card.innerHTML = numbersBigArray[i];
+        card.className = 'card';
+        grid.appendChild(card);
+    };
+};
+function createBoard() {
+    if (gridChosen === 'small') {
+        createSmallBoard();
+    } else if (gridChosen === 'big') {
+        createBigBoard();
+    } else {
+        console.log('no grid size chosen')
+    };
+};
 
-    createSmallBoard();
-})
+// start game button
+startGameBtn.addEventListener('click', () => {
+    createPlayersCards();
+    createBoard();
+});
